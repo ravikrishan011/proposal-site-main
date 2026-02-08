8074fbcb2fb0fff09b675219f46e11b436e954dd
ravikrishan011 <rp4080953@gmail.com>
2026-02-08 14:35:58 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,19 @@
+import "./globals.css"
+import BackgroundMusic from "@/components/BackgroundMusic"
+
+export const metadata = {
+  title: "Something Just for You!",
+  description: "A little surprise made just for you, open it with a smile ❤️",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased">
+        <BackgroundMusic />
+        {children}
+      </body>
+    </html>
+  )
+}
 
